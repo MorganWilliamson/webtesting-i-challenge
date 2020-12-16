69b1d0570d0b7bc5399c6be889bccf7d55f55cab
MorganWilliamson <mwilliamson7695@gmail.com>
2020-12-16 16:49:14 -0600

    Setting up initial tests and functions.

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -13,7 +13,13 @@ function fail(item) {
   return { ...item };
 }
 
+// 1) Take in an item object. 
+// 2) Set the durablitiy of the item to 100. 
+// 3) Return the repaired item. 
 function repair(item) {
+  if (item.durability < 100) {
+    item.durability = 100;
+  }
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,11 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+describe('Sanity Checks:', () => {
+    it('check 1', () => {
+        expect(2 + 2).toBe(4);
+    });
+    test('check 2', () => {
+        expect({}).toEqual({})
+        expect({}).not.toBe({})
+    })
+})
